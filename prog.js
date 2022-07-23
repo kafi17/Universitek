6d650b5769f3122c5ff5af322e2e2625a98246b9
kafi17 <40305628+kafi17@users.noreply.github.com>
2022-07-23 01:52:59 -0400

    Universitek
    Pagina para encontrar universidades

---
+++ prog.js
@@ -0,0 +1,25 @@
+
+function Foro(){
+    location.href="pag/foro.html";
+}
+
+function PagPrincipal(){
+    location.href="../index.html";
+}
+
+function ConoceUniversidades(){
+    location.href="pag/conoceUniv.html";
+}
+
+function PreguntasFrecuentes(){
+    location.href="pag/preguntasFrecuentes.html";
+}
+
+function Contactos(){
+    location.href="pag/contactos.html";
+}
+
+function Becas(){
+    location.href="pag/becas.html";
+}
+
